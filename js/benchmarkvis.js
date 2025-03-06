30d7bbe1e870fffee06a8af154171969bbb94784
lorena-b <lorena.buciu@mail.utoronto.ca>
2025-03-06 14:58:39 -0500

    vis 4 barplots

--- js/benchmarkvis.js
+++ js/benchmarkvis.js
@@ -1,24 +1,35 @@
-// const FILL_COLOR = "#f8f9fa";
-// const VIS_HEIGHT = 1000;
+/**
+ * Visualization for LLM benchmarks
+ */
+const BM_FILL_COLOR = "#f8f9fa";
+
 class BenchmarkVis {
-    constructor(_parentElement, _data) {
+    constructor(_parentElement, _data, _categories) {
         this.parentElement = _parentElement;
         this.data = _data;
+        this.displayData = _data;
+        this.categories = _categories;
+        this.averageData = [];
         this.initVis();
     }
 
     initVis() {
         let vis = this;
 
-        vis.margin = { top: 10, right: 10, bottom: 10, left: 10 };
+        vis.margin = { top: 10, right: 0, bottom: 10, left: 0 };
 
         (vis.width =
             document.getElementById(vis.parentElement).getBoundingClientRect()
                 .width -
             vis.margin.left -
             vis.margin.right),
-            (vis.height = 750 - vis.margin.top - vis.margin.bottom);
-            console.log(document.getElementById(vis.parentElement).getBoundingClientRect().width);
+            (vis.height =
+                document
+                    .getElementById(vis.parentElement)
+                    .getBoundingClientRect().width -
+                vis.margin.top -
+                vis.margin.bottom);
+
         vis.svg = d3
             .select(`#${vis.parentElement}`)
             .append("svg")
@@ -34,28 +45,184 @@ class BenchmarkVis {
                 "translate(" + vis.margin.left + "," + vis.margin.top + ")"
             );
 
-        const radius = 95
-        const numCircles = vis.data.length;
-        const totalCircleWidth = numCircles * (2 * radius);
+        vis.radius = 95;
+        const numCircles = vis.categories.length;
+        const totalCircleWidth = numCircles * (2 * vis.radius);
         const totalSpacing = vis.width - totalCircleWidth;
-        const spacing = totalSpacing / (numCircles + 1);
+        vis.spacing = totalSpacing / (numCircles + 1);
 
-        // init benchmark circles
-        vis.svg
+        // draw bars
+        vis.avgBars = vis.svg.append("g").selectAll("path");
+        vis.paramBars = vis.svg.append("g").selectAll("path");
+        vis.co2Bars = vis.svg.append("g").selectAll("path");
+
+        // init category circles
+        vis.circles = vis.svg
             .selectAll(".benchmark-circle")
-            .data(vis.data)
+            .data(vis.categories)
             .enter()
             .append("circle")
             .attr("class", "benchmark-circle")
-            .attr("cx", (d, i) => spacing + radius + i * (2 * radius + spacing))
+            .attr(
+                "cx",
+                (d, i) =>
+                    vis.spacing +
+                    vis.radius +
+                    i * (2 * vis.radius + vis.spacing)
+            )
             .attr("cy", vis.height / 4)
-            .attr("r", radius)
-            .attr("fill", "#f8f9fa");
+            .attr("r", vis.radius)
+            .attr("fill", BM_FILL_COLOR)
+            .style("z-index", 10);
+
+        vis.svg
+            .selectAll(".benchmark-text")
+            .data(vis.categories)
+            .enter()
+            .append("text")
+            .attr("class", "benchmark-text")
+            .attr(
+                "x",
+                (d, i) =>
+                    vis.spacing +
+                    vis.radius +
+                    i * (2 * vis.radius + vis.spacing)
+            )
+            .attr("y", vis.height / 4)
+            .attr("text-anchor", "middle")
+            .attr("alignment-baseline", "middle")
+            .attr("fill", "black")
+            .text((d) => d);
+
+        (vis.innerRadius = 80),
+            (vis.outerRadius = Math.min(vis.width, vis.height) / 2);
+
+        vis.x = d3.scaleBand().range([0, 2 * Math.PI]).padding(0.05);
+        vis.y = d3.scaleRadial().range([vis.innerRadius, vis.outerRadius]);
+
+        vis.avgColor = d3
+            .scaleSequential(d3.interpolateInferno)
+
+        vis.paramColor = d3.scaleSequential(d3.interpolateViridis);
+
+        vis.co2Color = d3.scaleSequential(d3.interpolateMagma);
 
         vis.wrangleData();
     }
 
-    wrangleData() {}
+    wrangleData() {
+        let vis = this;
 
-    updateVis() {}
+        // // // sort data
+        // vis.data = vis.data.sort((a, b) => d3.ascending(a.Average, b.Average));
+        // console.log(vis.data);
+
+        // clean data
+        vis.displayData.forEach((d) => {
+            d.params = d["#Params (B)"];
+            d.co2cost = d["CO cost (kg)"];
+        });
+
+        vis.displayData = vis.displayData.slice(0, 75);
+        vis.updateVis();
+    }
+
+    updateVis() {
+        let vis = this;
+
+        vis.scale = 0.4
+
+        vis.avgX = vis.x.domain(vis.displayData.map((d) => d["eval_name"]));
+        vis.avgY = vis.y.domain([0, d3.max(vis.displayData, (d) => d.Average)]);
+
+        vis.avgArc = d3
+            .arc()
+            .innerRadius(vis.innerRadius)
+            .outerRadius((d) => vis.avgY(d.Average) * vis.scale)
+            .startAngle((d) => vis.avgX(d["eval_name"]))
+            .endAngle((d) => vis.avgX(d["eval_name"]) + vis.x.bandwidth())
+            .padAngle(0.01)
+            .padRadius(vis.height / 2);
+
+        vis.avgColor.domain([0, d3.max(vis.displayData, (d) => d.Average)]);
+
+        vis.avgBars
+            .data(vis.displayData)
+            .enter()
+            .append("path")
+            .attr("fill", (d) => vis.avgColor(d.Average))
+            .attr("stroke", (d) => vis.avgColor(d.Average))
+            .merge(vis.avgBars)
+            .attr("d", vis.avgArc)
+            .attr(
+                "transform",
+                `translate(${
+                    vis.spacing +
+                    vis.radius +
+                    0 * (2 * vis.radius + vis.spacing)
+                }, ${vis.height / 4})`
+            );
+
+        vis.paramX = vis.x.domain(vis.displayData.map((d) => d["eval_name"]));
+        vis.paramY = vis.y.domain([0, d3.max(vis.displayData, (d) => d.params)]);
+
+        vis.paramArcs = d3
+            .arc()
+            .innerRadius(vis.innerRadius)
+            .outerRadius((d) => vis.paramY(d.params) * vis.scale)
+            .startAngle((d) => vis.paramX(d["eval_name"]))
+            .endAngle((d) => vis.paramX(d["eval_name"]) + vis.x.bandwidth())
+            .padAngle(0.01)
+            .padRadius(vis.height / 2);
+
+        vis.paramColor.domain([0, d3.max(vis.displayData, (d) => d.params)]);
+
+        vis.paramBars
+            .data(vis.displayData)
+            .enter()
+            .append("path")
+            .attr("fill", (d) => vis.paramColor(d.params))
+            .attr("stroke", (d) => vis.paramColor(d.params))
+            .merge(vis.paramBars)
+            .attr("d", vis.paramArcs)
+            .attr(
+                "transform",
+                `translate(${
+                    vis.spacing +
+                    vis.radius +
+                    1 * (2 * vis.radius + vis.spacing)
+                }, ${vis.height / 4})`
+            );
+
+        vis.co2X = vis.x.domain(vis.displayData.map((d) => d["eval_name"]));
+        vis.co2Y = vis.y.domain([0, d3.max(vis.displayData, (d) => d.co2cost)]);
+
+        vis.arc = d3
+            .arc()
+            .innerRadius(vis.innerRadius)
+            .outerRadius((d) => vis.co2Y(d.co2cost) * vis.scale)
+            .startAngle((d) => vis.co2X(d["eval_name"]))
+            .endAngle((d) => vis.co2X(d["eval_name"]) + vis.x.bandwidth())
+            .padAngle(0.01)
+            .padRadius(vis.height / 2);
+
+        vis.co2Color.domain([0, d3.max(vis.displayData, (d) => d.co2cost)]);
+
+        vis.co2Bars
+            .data(vis.displayData)
+            .enter()
+            .append("path")
+            .attr("fill", (d) => vis.co2Color(d.co2cost))
+            .attr("stroke", (d) => vis.co2Color(d.co2cost))
+            .merge(vis.co2Bars)
+            .attr("d", vis.arc)
+            .attr(
+                "transform",
+                `translate(${
+                    vis.spacing +
+                    vis.radius +
+                    2 * (2 * vis.radius + vis.spacing)
+                }, ${vis.height / 4})`
+            );
+    }
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 // Load data
 let promises = [
     d3.json("data/barriers.json"), 
-    d3.json("data/LLMStats_ChavezTamales_Data.json")
+    d3.json("data/LLMStats_ChavezTamales_Data.json"),
+    d3.json("data/open_data.json")
 ];
 
 Promise.all(promises)
     .then(function (data) {
-        console.log(data);
         renderVisualizations(data);
     })
     .catch(function (error) {
@@ -14,11 +14,14 @@ Promise.all(promises)
     });
 
 function renderVisualizations(data) {
-   
     BarrierData = data[0];
     LLMStatsData = data[1];
+    OpenData = data[2];
+
+    benchmarkCategories = ["Average Benchmark", "Parameters Per Billion", "CO2 Cost"];
+
     let barriersToEntryVis = new BarriersVis("barriers-vis", BarrierData);
-    // let benchmarkVis = new BenchmarkVis("benchmark-vis", [1, 2, 3]);
+    let benchmarkVis = new BenchmarkVis("benchmark-vis", OpenData, benchmarkCategories);
     let economicVis = new EconomicVis("economic-vis", LLMStatsData);
     let performanceVis = new PerformanceVis("performance-vis", LLMStatsData);
 }
