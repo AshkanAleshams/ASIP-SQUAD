a1098f6941030ccd5ee75c142e2bab2b3513f994
lorena-b <lorena.buciu@mail.utoronto.ca>
2025-03-13 15:31:58 -0400

    add scroll animations

--- js/animations.js
+++ js/animations.js
@@ -11,17 +11,83 @@ dotNav("section", "easeInOutQuint");
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
-// fade box transition
-gsap.fromTo(".fade-box", {
-    opacity: 0,
-}, {
-    opacity: 1,
-    duration: 1,
+// pin the page until all content below apears
+let tl = gsap.timeline({
     scrollTrigger: {
-        trigger: ".fade-box",
-        start: "top 80%",
-        end: "top 50%",
+        trigger: "#page1",
+        start: "top top",
+        end: "bottom top",
         scrub: true,
+        pin: true,
     },
 });
+//reveal content
+tl.from("#main-content", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+});
 
+// pin page 5 and reveal the vis
+let tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#page5",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        pin: true,
+    },
+});
+//reveal content
+tl2.from("#economic-vis", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+});
+
+let tl3 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#page7",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        pin: true,
+    },
+});
+//reveal content
+tl3.from("#performance-vis", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+});
+
+let tl4 = gsap.timeline({
+    scrollTrigger: {
+        trigger: "#page9",
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+        pin: true,
+    },
+});
+//reveal content
+tl4.from("#compare-vis", {
+    opacity: 0,
+    y: 50,
+    duration: 1,
+});
+
+gsap.utils.toArray(".animate-on-scroll").forEach((section) => {
+    gsap.from(section, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        scrollTrigger: {
+            trigger: section,
+            start: "top 80%",
+            end: "top 50%",
+            scrub: true,
+            toggleActions: "play reverse play reverse",
+        },
+    });
+});
