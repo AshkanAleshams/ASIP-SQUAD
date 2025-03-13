eeaefc577cc91ff8f04c936a940a264223e05fe1
lorena-b <lorena.buciu@mail.utoronto.ca>
2025-03-12 23:50:23 -0400

    fix window scroll

--- js/helpers.js
+++ js/helpers.js
@@ -2,9 +2,9 @@
 let backToTopButton = document.getElementById("back-to-top");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
+window.addEventListener("scroll", function() {
     scrollFunction();
-};
+});
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
